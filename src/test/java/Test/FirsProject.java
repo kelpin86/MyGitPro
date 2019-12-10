340242f0fbd174290248779ba404f250a02cb9f3
mustafa <kelpin86@gmail.ocm>
2019-12-10 11:27:34 +0000

    love java

---
+++ src/test/java/Test/FirsProject.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class FirsProject {
+    @Test
+    public void MyCommit(){
+        System.out.println("Git is cool");
+        System.out.println("We love Java and Git");
+    }
+}
--- src/test/java/Test/MyGit.java
+++ src/test/java/Test/MyGit.java
@@ -3,5 +3,6 @@ package Test;
 public class MyGit {
     public static void main(String[]args){
         System.out.println("I Love Java");
+
     }
 }
